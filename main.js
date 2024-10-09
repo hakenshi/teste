8e0afc552127f0d0c121537d586a387178eb4fb0
hakenshi <fkdias025@gmail.com>
2024-10-09 15:48:48 -0300

    init

--- main.js
+++ main.js
@@ -2,17 +2,18 @@ const cameraButton = document.querySelector('#start-camera')
 const video = document.querySelector("#video")
 const takePicture = document.querySelector("#click-photo")
 const canvas = document.querySelector("#canvas")
+let stream
 
 cameraButton.addEventListener("click", async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({video: true, audio: false})
+  stream = await navigator.mediaDevices.getUserMedia({video: true, audio: false})
   video.srcObject = stream
 })
 
 takePicture.addEventListener('click', () => {
   canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height)
-
   const imageUrl = canvas.toDataURL('images/jpeg')
-
-  console.log(imageUrl);
-  
+  if(stream){
+    stream.getTracks().forEach(track => track.stop())
+    }
+  video.srcObject = null
 })
