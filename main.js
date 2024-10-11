912ab04905d4c5087b58ac57c4cb620a6dd04558
hakenshi <fkdias025@gmail.com>
2024-10-11 13:45:10 -0300

    modified:   index.html 	modified:   main.js

--- main.js
+++ main.js
@@ -1,19 +1,26 @@
-const cameraButton = document.querySelector('#start-camera')
-const video = document.querySelector("#video")
-const takePicture = document.querySelector("#click-photo")
-const canvas = document.querySelector("#canvas")
-let stream
+// const cameraButton = document.querySelector('#start-camera')
+// const video = document.querySelector("#video")
+// const takePicture = document.querySelector("#click-photo")
+// const canvas = document.querySelector("#canvas")
+// let stream
 
-cameraButton.addEventListener("click", async () => {
-  stream = await navigator.mediaDevices.getUserMedia({video: true, audio: false})
-  video.srcObject = stream
-})
+// cameraButton.addEventListener("click", async () => {
+//   stream = await navigator.mediaDevices.getUserMedia({video: true, audio: false})
+//   video.srcObject = stream
+// })
 
-takePicture.addEventListener('click', () => {
-  canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height)
-  const imageUrl = canvas.toDataURL('images/jpeg')
-  if(stream){
-    stream.getTracks().forEach(track => track.stop())
-    }
-  video.srcObject = null
+// takePicture.addEventListener('click', () => {
+//   canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height)
+//   const imageUrl = canvas.toDataURL('images/jpeg')
+//   if(stream){
+//     stream.getTracks().forEach(track => track.stop())
+//     }
+//   video.srcObject = null
+// })
+
+const openDialog = document.querySelector('.open-button')
+const dialog = document.querySelector('dialog')
+
+openDialog.addEventListener('click', () => {
+  dialog.showModal()
 })
